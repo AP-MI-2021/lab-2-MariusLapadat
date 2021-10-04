cfa9735792a9442e20a4fd38d453348e30c836d1
Marius Cristian Lapadat <lapadatmarius23@gmail.com>
2021-10-04 21:14:31 +0300

    -

---
+++ main/main.py
@@ -0,0 +1,13 @@
+def get_largest_prime_below(n):
+    '''
+    Subprogramul returneaza cel mai mare numar prim mai mic decat n
+    :param n-numar intreg:
+    :return n-numar intreg:
+    '''
+
+
+
+if __name__ == '__main__':
+    print('PyCharm')
+
+
